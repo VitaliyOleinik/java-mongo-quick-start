e877c7dd6fa63183e04ce22e3d7a731c45184efa
Vitaliy Oleinik <oleynik624@gmail.com>
2023-11-21 23:58:03 +0600

    Initial commit

---
+++ src/main/java/com/example/javamongoquickstart/JavaMongoQuickStartApplication.java
@@ -0,0 +1,13 @@
+package com.example.javamongoquickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMongoQuickStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMongoQuickStartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javamongoquickstart/JavaMongoQuickStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javamongoquickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMongoQuickStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
